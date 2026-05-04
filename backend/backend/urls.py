4e81d410b2bcaeee7ee2fa6eb9886bec8ee32441
Екатарина Жукова <yekatya.zhu888@gmail.com>
2026-05-04 18:57:49 +0700

    Add actions

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,9 @@ from api import views
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
@@ -10,3 +13,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
